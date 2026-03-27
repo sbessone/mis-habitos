e14610e64955e14fc476e65debaf6b21aab12d36
sbessone <sbessone@gmail.com>
2026-03-27 10:36:10 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <title></title>
+  <meta name="Generator" content="Cocoa HTML Writer">
+  <meta name="CocoaVersion" content="2487.5">
+  <style type="text/css">
+    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 13.0px Courier; -webkit-text-stroke: #000000}
+    p.p2 {margin: 0.0px 0.0px 0.0px 0.0px; font: 13.0px Courier; -webkit-text-stroke: #000000; min-height: 16.0px}
+    span.s1 {font-kerning: none}
+  </style>
+</head>
+<body>
+<p class="p1"><span class="s1">import React from 'react'</span></p>
+<p class="p1"><span class="s1">import ReactDOM from 'react-dom/client'</span></p>
+<p class="p1"><span class="s1">import App from './App.jsx'</span></p>
+<p class="p2"><span class="s1"></span><br></p>
+<p class="p1"><span class="s1">ReactDOM.createRoot(document.getElementById('root')).render(</span></p>
+<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>&lt;React.StrictMode&gt;</span></p>
+<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>&lt;App /&gt;</span></p>
+<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>&lt;/React.StrictMode&gt;</span></p>
+<p class="p1"><span class="s1">)</span></p>
+</body>
+</html>
